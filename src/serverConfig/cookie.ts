300b3636444377e05073160c4b4006d970cac5a0
Mansoor shakir <mansoorshakir642@gmail.com>
2022-08-16 14:55:33 +0530

    done

--- src/serverConfig/cookie.ts
+++ src/serverConfig/cookie.ts
@@ -6,7 +6,7 @@ const { SERVER_SECURE, SERVER_CLIENT_DOMAIN } = config.get('SERVER') as SERVER;
 
 const getCookieConfig = (expireTime: number): CookieOptions => ({
     httpOnly: true,
-    secure: SERVER_SECURE,
+    // secure: SERVER_SECURE,
     maxAge: expireTime,
     sameSite: 'strict',
     domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
