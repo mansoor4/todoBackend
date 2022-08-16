acb545c0aa2e1dc20527dd25d81202c40f0d9a12
Mansoor shakir <mansoorshakir642@gmail.com>
2022-08-16 13:58:17 +0530

    done

--- dest/serverConfig/cookie.js
+++ dest/serverConfig/cookie.js
@@ -4,11 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = __importDefault(require("config"));
-var SERVER_SECURE = config_1.default.get('SERVER').SERVER_SECURE;
+var _a = config_1.default.get('SERVER'), SERVER_SECURE = _a.SERVER_SECURE, SERVER_CLIENT_DOMAIN = _a.SERVER_CLIENT_DOMAIN;
 var getCookieConfig = function (expireTime) { return ({
     httpOnly: true,
     secure: SERVER_SECURE,
     maxAge: expireTime,
     sameSite: 'strict',
+    domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
 }); };
 exports.default = getCookieConfig;
--- src/serverConfig/cookie.ts
+++ src/serverConfig/cookie.ts
@@ -2,13 +2,14 @@ import config from 'config';
 import { CookieOptions } from 'express';
 import { SERVER } from '../types/config';
 
-const { SERVER_SECURE } = config.get('SERVER') as SERVER;
+const { SERVER_SECURE, SERVER_CLIENT_DOMAIN } = config.get('SERVER') as SERVER;
 
 const getCookieConfig = (expireTime: number): CookieOptions => ({
     httpOnly: true,
     secure: SERVER_SECURE,
     maxAge: expireTime,
     sameSite: 'strict',
+    domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
 });
 
 export default getCookieConfig;
