4a56b129acde15eb734b7c0b5f06f2b7f6e3912d
Mansoor shakir <mansoorshakir642@gmail.com>
2022-08-16 16:10:08 +0530

    done

--- src/serverConfig/cookie.ts
+++ src/serverConfig/cookie.ts
@@ -8,7 +8,7 @@ const getCookieConfig = (expireTime: number): CookieOptions => ({
     httpOnly: true,
     secure: SERVER_SECURE,
     maxAge: expireTime,
-    sameSite: SERVER_SECURE ? 'none' : 'strict',
+    sameSite: SERVER_SECURE ? 'strict' : 'strict',
     domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
 });
 
