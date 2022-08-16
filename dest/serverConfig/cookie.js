5339ef640fda379d081bf8a5c75f3b7d414721be
Mansoor shakir <mansoorshakir642@gmail.com>
2022-08-16 15:27:10 +0530

    done

--- dest/serverConfig/cookie.js
+++ dest/serverConfig/cookie.js
@@ -1,15 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var config_1 = __importDefault(require("config"));
-var _a = config_1.default.get('SERVER'), SERVER_SECURE = _a.SERVER_SECURE, SERVER_CLIENT_DOMAIN = _a.SERVER_CLIENT_DOMAIN;
+// import { SERVER } from '../types/config';
+// const { SERVER_SECURE, SERVER_CLIENT_DOMAIN } = config.get('SERVER') as SERVER;
 var getCookieConfig = function (expireTime) { return ({
     httpOnly: true,
-    secure: SERVER_SECURE,
+    secure: false,
     maxAge: expireTime,
-    sameSite: 'none',
-    domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
+    sameSite: 'strict',
+    // domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
 }); };
 exports.default = getCookieConfig;
--- src/serverConfig/cookie.ts
+++ src/serverConfig/cookie.ts
@@ -1,15 +1,15 @@
-import config from 'config';
+// import config from 'config';
 import { CookieOptions } from 'express';
-import { SERVER } from '../types/config';
+// import { SERVER } from '../types/config';
 
-const { SERVER_SECURE, SERVER_CLIENT_DOMAIN } = config.get('SERVER') as SERVER;
+// const { SERVER_SECURE, SERVER_CLIENT_DOMAIN } = config.get('SERVER') as SERVER;
 
 const getCookieConfig = (expireTime: number): CookieOptions => ({
     httpOnly: true,
-    secure: SERVER_SECURE,
+    secure: false,
     maxAge: expireTime,
-    sameSite: 'none',
-    domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
+    sameSite: 'strict',
+    // domain: SERVER_SECURE ? SERVER_CLIENT_DOMAIN : 'localhost',
 });
 
 export default getCookieConfig;
